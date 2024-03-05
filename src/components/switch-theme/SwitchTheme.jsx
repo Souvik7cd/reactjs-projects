f6021b7d891cc1b285d6211372e3c37eab509bc9
Souvik Datta <dattasouvik1997@gmail.com>
2024-03-05 23:46:29 +0530

    Added dark light theme switch component

---
+++ src/components/switch-theme/SwitchTheme.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./SwitchTheme.css";
+import useLocalStorage from "./useLocalStorage";
+
+const SwitchTheme = () => {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div className="wrapper theme-bg" data-theme={theme}>
+      <h1 style={{ fontWeight: "lighter", margin: "0" }}>Hello World!</h1>
+      <p>How are you?</p>
+      <button
+        className={`theme-btn ${
+          theme === "light" ? "theme-btn-dark" : "theme-btn-light"
+        }`}
+        onClick={toggleTheme}
+      >
+        Change Theme
+      </button>
+    </div>
+  );
+};
+
+export default SwitchTheme;
