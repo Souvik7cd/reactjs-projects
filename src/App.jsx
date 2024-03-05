87c631ede5dd81f7a06f046117f8b9c6fa501c73
Souvik Datta <dattasouvik1997@gmail.com>
2024-03-05 22:45:06 +0530

    Added QR Code Generator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Accordion from './components/accordion/Accordion'
 import ImageSlider from './components/image-slider/ImageSlider'
 import LoadMore from './components/load-more/LoadMore'
+import QrCodeGenerator from './components/qr-code-generator/QrCodeGenerator'
 import RandomColor from './components/random-color/RandomColor'
 import StarRating from './components/star-rating/StarRating'
 import TreeView from './components/tree-view/TreeView'
@@ -18,6 +19,7 @@ function App() {
       <ImageSlider url="https://picsum.photos/v2/list" page={10} limit={10}/>
       <LoadMore url="https://dummyjson.com/products" limit={20} />
       <TreeView menus={menus} />
+      <QrCodeGenerator />
     </>
   )
 }
---
+++ src/components/qr-code-generator/QrCodeGenerator.jsx
@@ -0,0 +1,35 @@
+import QRCode from "react-qr-code";
+import "./QrCodeGenerator.css";
+import { useState } from "react";
+
+const QrCodeGenerator = () => {
+  const [text, setText] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="wrapper bg-qrcode">
+      <h1 className="qr-heading">QR Code Generator</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setText(inputValue);
+          setInputValue("");
+        }}
+        className="form-field-inline"
+      >
+        <input
+          id="qr-text"
+          name="qr-text"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Type your message here..."
+        />
+        <button disabled={!(inputValue && inputValue.trim() !== "")} type="submit">Generate</button>
+      </form>
+      <div className="qr-output">{text && <QRCode value={text} />}</div>
+    </div>
+  );
+};
+
+export default QrCodeGenerator;
